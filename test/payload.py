27aa7d4f17265b008f99fab939019a0dd4625b41
Piotr Sarna <sarna@chiselstrike.com>
2023-03-06 15:09:55 +0100

    run on 0.0.0.0 by default

--- test/payload.py
+++ test/payload.py
@@ -7,7 +7,7 @@ toaddr  = "testto@example.com"
 msg = f"From: {fromaddr}\r\nTo: {toaddr}\r\n\r\n"
 msg += "test \nmail\n goodbye\n"
 
-server = smtplib.SMTP('localhost', port=8080)
+server = smtplib.SMTP('localhost', port=8088)
 server.set_debuglevel(1)
 server.sendmail(fromaddr, toaddr, msg)
 server.quit()
