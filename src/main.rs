1c27bf0aca8260c80cd1946893578da901c9ba85
Piotr Sarna <sarna@chiselstrike.com>
2023-04-18 14:18:26 +0200

    Simplify main

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind(&addr).await?;
     tracing::info!("Listening on: {}", addr);
 
-    let local = tokio::task::LocalSet::new();
-
     // Task for deleting old mail
-    local.spawn_local(async move {
+    tokio::task::LocalSet::new().spawn_local(async move {
         let db = match eatmail::database::Client::new().await {
             Ok(db) => db,
             Err(e) => {
@@ -27,7 +25,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 return;
             }
         };
-        let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(10));
+        let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(3600));
+        interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
         loop {
             interval.tick().await;
             tracing::info!("Deleting old mail");
@@ -42,14 +41,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let (stream, addr) = listener.accept().await?;
         tracing::info!("Accepted a connection from {}", addr);
 
-        local
+        tokio::task::LocalSet::new()
             .run_until(async move {
-                let conn = tokio::task::spawn_local(async move {
-                    let mut smtp = smtp::Server::new(stream).await?;
-                    smtp.greet().await?;
-                    smtp.serve().await
-                });
-                conn.await
+                let mut smtp = smtp::Server::new(stream).await?;
+                smtp.greet().await?;
+                smtp.serve().await
             })
             .await
             .ok();
